df0d775caf2392ad97672d08ac346c7f74e627ef
장현석[W Stat] <hyunsuk.jang@webtoonscorp.com>
2022-12-09 00:42:51 +0900

    java 기본 기능으로 회원정보 등록 및 조회 서비스 개발

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.Properties;
+
+public class MemberServiceImpl implements MemberService {
+
+    private MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
